c5f69937cf4685331ad3f3a78da209ae3d753e45
Dhannyphantom <youngskillzzz@gmail.com>
2022-09-08 00:20:15 +0100

    Created a Footer component

---
+++ components/Footer/Footer.js
@@ -0,0 +1,63 @@
+import styles from "./Footer.module.css";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+
+export default function Footer() {
+  return (
+    <footer className={styles.footer}>
+      <div className={styles.grid}>
+        <div>
+          <p>Back to top</p>
+          <p>Contact or Leave a message</p>
+          <p>Hire Me!</p>
+        </div>
+        <div>
+          <p>My Services</p>
+          <p>Languages</p>
+        </div>
+        <div className={styles.icon_container}>
+          <a
+            href="https://github.com/Dhannyphantom"
+            target="_blank"
+            rel="noreferrer"
+          >
+            <FontAwesomeIcon
+              size="sm"
+              className={styles.icon}
+              icon={["fab", "github"]}
+            />
+          </a>
+          <a
+            href="https://wa.me/+23407081713909"
+            target="_blank"
+            rel="noreferrer"
+          >
+            <FontAwesomeIcon
+              size="sm"
+              className={styles.icon}
+              icon={["fab", "whatsapp"]}
+            />
+          </a>
+          <a
+            href="https://www.linkedin.com/in/daniel-olojo-b61904243"
+            target="_blank"
+            rel="noreferrer"
+          >
+            <FontAwesomeIcon
+              size="sm"
+              className={styles.icon}
+              icon={["fab", "linkedin-in"]}
+            />
+          </a>
+          <a target="_blank" rel="noreferrer">
+            <FontAwesomeIcon
+              size="sm"
+              className={styles.icon}
+              icon={["fab", "twitter"]}
+            />
+          </a>
+        </div>
+      </div>
+      <p> &copy; Copyrights reserved @codewithdhanny 2022 </p>
+    </footer>
+  );
+}
